28c074dcfe67a97e9c4a07921228db9acf8dec6c
ChantMyZeal <Kevin_cmz4570@outlook.com>
2024-03-28 13:19:01 +0800

    菜品分页查询功能开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
@@ -10,5 +12,13 @@ public interface DishService {
      * @param dishDTO 菜品DTO
      */
     void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO 菜品分页查询DTO
+     * @return 返回封装好的PageResult对象
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 
 }
