468d0d713b7678102b5ead5040b45a8efdd12c3a
ChantMyZeal <Kevin_cmz4570@outlook.com>
2024-04-02 23:24:30 +0800

    缓存菜品功能开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -26,6 +28,21 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 清理缓存数据
+     *
+     * @param pattern 字符串匹配
+     */
+    private void cleanCache(String pattern) {// todo 优化到service层中
+        //将指定的缓存数据，即pattern匹配的key清理掉
+        Set<String> keys = redisTemplate.keys(pattern);
+        if (keys != null && !keys.isEmpty()) {
+            redisTemplate.delete(keys);
+        }
+    }
 
     /**
      * 新增菜品
@@ -90,9 +107,13 @@ public class DishController {
      * @return 返回统一响应结果
      */
     @PutMapping
-    @Operation(summary = "修改菜品")
+    @Operation(summary = "修改菜品")// todo 考虑是否添加启售中的菜品无法修改的逻辑
     public Result<String> update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品：{}", dishDTO);
+
+        //将所有的菜品缓存数据清理掉
+        cleanCache("dish_*");
+
         dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
@@ -121,6 +142,10 @@ public class DishController {
     @Operation(summary = "启售或禁售菜品")
     public Result<String> startOrStop(@PathVariable Integer status, Long id) {
         log.info("启售或禁售菜品：{},{}", status, id);
+
+        //将所有的菜品缓存数据清理掉
+        cleanCache("dish_*");
+
         dishService.startOrStop(status, id);
         return Result.success();
     }
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.user;
 
+import com.alibaba.fastjson2.JSON;
 import com.sky.constant.StatusConstant;
 import com.sky.entity.Dish;
 import com.sky.result.Result;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,11 @@ import java.util.List;
 @Slf4j
 @Tag(name = "C端-菜品浏览接口")
 public class DishController {
+
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 根据分类ID查询菜品
@@ -32,12 +37,24 @@ public class DishController {
     @GetMapping("/list")
     @Operation(summary = "根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        //构造redis中的key，规则：dish_分类ID
+        String key = "dish_" + categoryId;
+        //查询redis中是否存在菜品数据
+        Object object = redisTemplate.opsForValue().get(key);
+        String jsonString = JSON.toJSONString(object);
+        List<DishVO> list = JSON.parseArray(jsonString, DishVO.class);
+
+        //如果存在，直接返回，无需查询数据库
+        if (list != null && !list.isEmpty()) {
+            return Result.success(list);
+        }
+
+        //如果不存在，需要查询数据库，将查询到的数据放入redis中，再返回
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
-        dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
-
-        List<DishVO> list = dishService.listWithFlavor(dish);
-
+        dish.setStatus(StatusConstant.ENABLE);//在数据库中查询起售中的菜品，并存入redis中
+        list = dishService.listWithFlavor(dish);
+        redisTemplate.opsForValue().set(key, list);
         return Result.success(list);
     }
 
